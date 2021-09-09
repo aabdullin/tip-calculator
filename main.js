a1b7ffaf0a929b1393a71db67b49b5e080d4cbeb
Artur <rutrika@gmail.com>
2021-09-08 21:20:18 -0700

    wip

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ class SplitBill {
         this.tipPerPerson = this.tipPerPerson.bind(this);
         this.tipTotal = this.tipTotal.bind(this);
         this.onTipClicked = this.onTipClicked.bind(this);
-        // this.selectedTip = this.selectedTip.bind(this);
+        this.selectedTip = this.selectedTip.bind(this);
     }
 
     numberOfPeople () {
@@ -32,24 +32,24 @@ class SplitBill {
 
     selectedTip () {
         document.getElementById("5_tip").onclick = function() {
-            onTipClicked(0.05);
+            this.onTipClicked(0.05);
         };
         document.getElementById("10_tip").onclick = function() {
-            onTipClicked(0.1);
+            this.onTipClicked(0.1);
         };
         document.getElementById("15_tip").onclick = function() {
-            onTipClicked(0.15);
+            this.onTipClicked(0.15);
         };
         document.getElementById("20_tip").onclick = function() {
-            onTipClicked(0.2);
+            this.onTipClicked(0.2);
         };
         document.getElementById("25_tip").onclick = function() {
-            onTipClicked(0.25);
+            this.onTipClicked(0.25);
         };
         document.getElementById("50_tip").onclick = function() {
-            onTipClicked(0.5);
+            this.onTipClicked(0.5);
         };
-        // document.getElementById("5_tip").o(0.05) => { this.onTipClicked(); };
+        // document.getElementById("5_tip").o(0.05) => { this.onTipClicked(0.5); };
         // document.getElementById("10_tip").o(0.1) => { this.onTipClicked(); };
         // document.getElementById("15_tip").o(0.15) => { this.onTipClicked(); };
         // document.getElementById("20_tip").o(0.2) => { this.onTipClicked(); };
