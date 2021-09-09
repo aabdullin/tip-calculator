1c519d4ba346a56ae2c1257816d5361dfcb4a3a1
Artur <rutrika@gmail.com>
2021-09-08 18:38:12 -0700

    wip

--- main.js
+++ main.js
@@ -4,33 +4,10 @@ class SplitBill {
         this.numberOfPeople = document.querySelector(".people-input").value;
         this.resultTip = document.querySelector("#displayTipPerP").value;
         this.resultTotal = document.querySelector("#displayTotalPerP").value;
-    }
-
-    selectedTip () {
-        document.getElementById("5_tip").onclick = function() {
-            tipPerPerson (0.05);
-            tipTotal (0.05)      
-        };
-        document.getElementById("10_tip").onclick = function() {
-            tipPerPerson (0.1);  
-            tipTotal (0.1)          
-        };
-        document.getElementById("15_tip").onclick = function() {
-            tipPerPerson (0.15); 
-            tipTotal (0.15)           
-        };
-        document.getElementById("20_tip").onclick = function() {
-            tipPerPerson (0.20); 
-            tipTotal (0.20)           
-        };
-        document.getElementById("25_tip").onclick = function() {
-            tipPerPerson (0.25); 
-            tipTotal (0.25)           
-        };
-        document.getElementById("50_tip").onclick = function() {
-            tipPerPerson (0.50); 
-            tipTotal (0.50)           
-        };
+        this.tipPerPerson = this.tipPerPerson.bind(this);
+        this.tipTotal = this.tipTotal.bind(this);
+        this.onTipClicked = this.onTipClicked.bind(this);
+        // this.selectedTip = this.selectedTip.bind(this);
     }
 
     numberOfPeople () {
@@ -48,6 +25,38 @@ class SplitBill {
         resultTotal.innerHTML = tipTotalPerP
     }
 
+    onTipClicked(tip) {
+        this.tipPerPerson (tip);
+        this.tipTotal (tip);
+     }
+
+    selectedTip () {
+        document.getElementById("5_tip").onclick = function() {
+            onTipClicked(0.05);
+        };
+        document.getElementById("10_tip").onclick = function() {
+            onTipClicked(0.1);
+        };
+        document.getElementById("15_tip").onclick = function() {
+            onTipClicked(0.15);
+        };
+        document.getElementById("20_tip").onclick = function() {
+            onTipClicked(0.2);
+        };
+        document.getElementById("25_tip").onclick = function() {
+            onTipClicked(0.25);
+        };
+        document.getElementById("50_tip").onclick = function() {
+            onTipClicked(0.5);
+        };
+        // document.getElementById("5_tip").o(0.05) => { this.onTipClicked(); };
+        // document.getElementById("10_tip").o(0.1) => { this.onTipClicked(); };
+        // document.getElementById("15_tip").o(0.15) => { this.onTipClicked(); };
+        // document.getElementById("20_tip").o(0.2) => { this.onTipClicked(); };
+        // document.getElementById("25_tip").o(0.25) => { this.onTipClicked(); };
+        // document.getElementById("50_tip").o(0.5) => { this.onTipClicked() };
+    }
+
     clear() {
         document.querySelector("bill-input").value = ""
         document.querySelector("people-input").value = ""
@@ -55,3 +64,4 @@ class SplitBill {
 }
 
 const bill = new SplitBill()
+bill.selectedTip()
