762baca4c05348df5d76521731d7ac8f27cd1903
Artur <rutrika@gmail.com>
2021-09-09 21:26:55 -0700

    wip

--- main.js
+++ main.js
@@ -1,28 +1,64 @@
 class SplitBill {
     constructor() {
-        this.bill = document.querySelector(".bill-input").value;
-        this.numberOfPeople = document.querySelector(".people-input").value;
-        this.resultTip = document.querySelector("#displayTipPerP").value;
-        this.resultTotal = document.querySelector("#displayTotalPerP").value;
+        // DO NOT GET THE VALUE OF SELECTOR INSIDE THE CONSTRUCTOR
+        // IT WILL ALWAYS BE BLANK
+        // EX.  let numOfPeople = document.querySelector(".people-input").value;
+
+        // DO GET THE HTML TAGS SO YOU CAN MANIPULATOR THEM IN THE FUTURE
+        // EX.  this.resultTip = document.querySelector("#displayTipPerP");
+
+        // DO BIND THE FUNCTIONS TO ENSURE THEY ALL GET THE RIGHT SCOPE WHEN THEY ARE CALLED
+        // EX.  this.init = this.init.bind(this);
+        this.resultTip = document.querySelector("#displayTipPerP");
+        this.resultTotal = document.querySelector("#displayTotalPerP");
+        this.getBill = this.getBill.bind(this);
+        this.getNumberOfPeople = this.getNumberOfPeople.bind(this)
         this.tipPerPerson = this.tipPerPerson.bind(this);
         this.tipTotal = this.tipTotal.bind(this);
         this.onTipClicked = this.onTipClicked.bind(this);
-        this.selectedTip = this.selectedTip.bind(this);
+        this.init = this.init.bind(this);
+
+        // INIT CAN BE CALLED HERE TOO
+        // this.init();
     }
 
-    numberOfPeople () {
+    // DO USE GET METHODS WHEN GETTING A VALUE FROM AN HTML INPUT
+    // EX. BELOW   
+    getNumberOfPeople () {
         let numOfPeople = document.querySelector(".people-input").value;
-        return numOfPeople
+        return Number(numOfPeople)
+    }
+
+    getBill () {
+        let getBill = document.querySelector(".bill-input").value;
+        return Number(getBill)
+    }
+
+    // DO USE SET METHODS WHEN SETTING A VALUE OF AN HTML INPUT
+    // EX. BELOW
+    setNumberOfPeople(value) {
+        document.querySelector(".people-input").value = value
+    }
+
+    setBill(value) {
+        document.querySelector(".bill-input").value = value
+    }
+
+    clear() {
+        this.setNumberOfPeople("")
+        this.setBill("")
+        this.resultTip.innerHTML = ""
+        this.resultTotal.innerHTML = ""
     }
 
     tipPerPerson (tip) {
-        let tipPerP = (this.bill * tip) / numberOfPeople ()
-        resultTip.innerHTML = tipPerP
+        let tipPerP = (this.getBill() * tip) / this.getNumberOfPeople ()
+        this.resultTip.innerHTML = tipPerP
     }
 
     tipTotal (tip) {
-        let tipTotalPerP = ((this.bill * tip) / numberOfPeople ()) + (this.bill / this.numberOfPeople() )
-        resultTotal.innerHTML = tipTotalPerP
+        let tipTotalPerP = ((this.getBill() * tip) / this.getNumberOfPeople ()) + (this.getBill() / this.getNumberOfPeople() )
+        this.resultTotal.innerHTML = tipTotalPerP
     }
 
     onTipClicked(tip) {
@@ -30,37 +66,40 @@ class SplitBill {
         this.tipTotal (tip);
      }
 
-    selectedTip () {
-        document.getElementById("5_tip").onclick = function() {
+    // DO USE AN INIT METHOD TO BIND EVENTS TO EVENT HANDLERS
+    // DO CALL THE INIT METHOD IMMEDIATELY AFTER THE CONSTRUCTOR
+    //    OR DO CALL THE INIT METHOD INSIDE THE CONSTRUCTOR ITSELF
+    init () {
+        document.getElementById("5_tip").onclick = () => {
             this.onTipClicked(0.05);
         };
-        document.getElementById("10_tip").onclick = function() {
+        document.getElementById("10_tip").onclick = () => {
             this.onTipClicked(0.1);
         };
-        document.getElementById("15_tip").onclick = function() {
+        document.getElementById("15_tip").onclick = () => {
             this.onTipClicked(0.15);
         };
-        document.getElementById("25_tip").onclick = function() {
+        document.getElementById("25_tip").onclick = () => {
             this.onTipClicked(0.25);
         };
-        document.getElementById("50_tip").onclick = function() {
+        document.getElementById("50_tip").onclick = () => {
             this.onTipClicked(0.5);
         };
-        // document.getElementById("5_tip").o(0.05) => { this.onTipClicked(0.5); };
-        // document.getElementById("10_tip").o(0.1) => { this.onTipClicked(); };
-        // document.getElementById("15_tip").o(0.15) => { this.onTipClicked(); };
-        // document.getElementById("20_tip").o(0.2) => { this.onTipClicked(); };
-        // document.getElementById("25_tip").o(0.25) => { this.onTipClicked(); };
-        // document.getElementById("50_tip").o(0.5) => { this.onTipClicked() };
+        document.getElementById("reset_button").onclick = () => {
+            this.clear()
+        };
+        // document.getElementById("5_tip").o(0.05) = () => { this.onTipClicked(0.5); };
+        // document.getElementById("10_tip").o(0.1) = () => { this.onTipClicked(); };
+        // document.getElementById("15_tip").o(0.15) = () => { this.onTipClicked(); };
+        // document.getElementById("20_tip").o(0.2) = () => { this.onTipClicked(); };
+        // document.getElementById("25_tip").o(0.25) = () => { this.onTipClicked(); };
+        // document.getElementById("50_tip").o(0.5) = () => { this.onTipClicked() };
     }
 
-    clear() {
-        document.querySelector("bill-input").value = ""
-        document.querySelector("people-input").value = ""
-    }
+
 }
 
 const bill = new SplitBill()
-bill.selectedTip()
+bill.init()
 
 
